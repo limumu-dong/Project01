4214b5ee577e3ecaed92578a699d6ef47728175f
limumu-dong <lilei19941221@163.com>
2020-03-29 16:02:09 +0800

    测试修改

--- src/com/lilei/java/HelloWorld.java
+++ src/com/lilei/java/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World");
         ArrayList list = new ArrayList();
+        System.out.println(list);
     }
 
     public  void method(){
